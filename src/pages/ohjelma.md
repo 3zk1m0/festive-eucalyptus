61d9f5e4d8e171812a67ba73a316369a5ab9203a
Sander Kallio <sander.kallio@tuni.fi>
2020-01-27 00:11:50 +0200

    fixed button

--- src/pages/ohjelma.md
+++ src/pages/ohjelma.md
@@ -1,9 +1,9 @@
 ---
-iframe_url: https://docs.google.com/spreadsheets/d/e/2PACX-1vTZpzKdjrPJQOchG9PEYd0BisoWpKcNaG8JafFc_MTW38bF3zewlaYBiME1t9fXJrPSABrMgTQi3jyp/pubhtml?widget=true&amp;headers=false&chrome=false
+iframe_url: https://docs.google.com/spreadsheets/d/e/2PACX-1vSVYQEhTn1c__OH-thZD0N3VvTwsEZWgohnMOQ-Cd0cTIavh_yP-lJgrO3OJZDJGpV5vCD2BfncjHIw/pubhtml?widget=true&amp;headers=false&chrome=false
 iframe_height: 900px
 iframe_width: 80%
-donwload_link: https://docs.google.com/spreadsheets/d/e/2PACX-1vTZpzKdjrPJQOchG9PEYd0BisoWpKcNaG8JafFc_MTW38bF3zewlaYBiME1t9fXJrPSABrMgTQi3jyp/pub?output=pdf
-button_text: Lataa aikataulu
+donwload_link: https://docs.google.com/spreadsheets/d/e/2PACX-1vSVYQEhTn1c__OH-thZD0N3VvTwsEZWgohnMOQ-Cd0cTIavh_yP-lJgrO3OJZDJGpV5vCD2BfncjHIw/pub?gid=562528686&single=true&output=pdf
+button_text: Lataa ohjelma
 template: google_sheets
 menus:
   main:
--- src/templates/google_sheets.js
+++ src/templates/google_sheets.js
@@ -9,8 +9,13 @@ export default class Page extends React.Component {
         return (
             <Layout {...this.props}>
               <div className="block-header google-sheets">
-                <form className="google-sheets-download" action={_.get(this.props, 'pageContext.frontmatter.donwload_link')}>
-                    <button type="submit">{_.get(this.props, 'pageContext.frontmatter.button_text')}</button>
+              <form className="google-sheets-download">
+              <a ref={_.get(this.props, 'pageContext.frontmatter.donwload_link')} >
+                  <button>
+                      {_.get(this.props, 'pageContext.frontmatter.button_text')}
+                      </button>
+                      </a>
+                
                 </form>  
                 <iframe src={_.get(this.props, 'pageContext.frontmatter.iframe_url')} width={_.get(this.props, 'pageContext.frontmatter.iframe_width')} height={_.get(this.props, 'pageContext.frontmatter.iframe_height')} allowfullscreen="" frameborder="0">Loading…</iframe>
               </div>
@@ -18,29 +23,3 @@ export default class Page extends React.Component {
         );
     }
 }
-/*
-{_.map(recent_posts, (post, post_idx) => (
-    <article key={post_idx} className="post post-card">
-      <div className="post-card-inside">
-        {_.get(post, 'frontmatter.thumb_img_path') && 
-        <Link className="post-card-thumbnail" to={safePrefix(_.get(post, 'url'))}>
-          <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
-        </Link>
-        }
-        <div className="post-card-content">
-          <header className="post-header">
-            <h3 className="post-title"><Link to={safePrefix(_.get(post, 'url'))} rel="bookmark">{_.get(post, 'frontmatter.title')}</Link></h3>
-            <h4 className="post-subtitle"><Link to={safePrefix(_.get(post, 'url'))} rel="bookmark">{_.get(post, 'frontmatter.speaker')}</Link></h4>
-          </header>
-          <div className="post-excerpt">
-            <p>{_.get(post, 'frontmatter.excerpt')}</p>
-          </div>
-          <footer className="post-meta">
-            <a  className="post-card-company-link"  href={_.get(post, 'frontmatter.company_url')}>
-              <img src={safePrefix(_.get(post, 'frontmatter.company_logo'))} alt={_.get(post, 'frontmatter.company')} />
-            </a>
-          </footer>
-        </div>
-      </div>
-    </article>
-    ))}*/
